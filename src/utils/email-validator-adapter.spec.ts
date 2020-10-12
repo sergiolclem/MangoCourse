2cf84b28e22f90aea5c9c8004ac4eeb10231ddd3
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-12 20:11:03 -0300

    test: ensure EmailValidatorAdapter calls Validator with correct param

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
+
+  test('Should call Validator with correct param', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('valid_email@mail.com')
+    expect(isEmailSpy).toHaveBeenLastCalledWith('valid_email@mail.com')
+  })
 })
