49897d1fb31a2cb2aa0dbb1e005197ccd7ffd8b7
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-17 15:16:54 -0300

    refactor: improve if statement on MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -18,7 +18,7 @@ export const MongoHelper = {
   },
 
   async getCollection (name: string): Promise<Collection> {
-    if (!this.client || !this.client.isConnected()) {
+    if (!this.client?.isConnected()) {
       await this.connect(this.uri)
     }
     return this.client.db().collection(name)
