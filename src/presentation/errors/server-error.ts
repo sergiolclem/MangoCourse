eb4b81889ae37aa38e3ef923e99b9aebf9af099d
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-17 22:03:19 -0300

    feat: ensure signupController pass the error to ServerError

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack: string) {
     super('Something went wrong, try again in a few minutes')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
