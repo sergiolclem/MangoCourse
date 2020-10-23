451084a67ffeb0608f05e0cc882d8948878410f0
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-22 21:17:47 -0300

    test: ensure JwtAdapter return a token if sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -25,4 +25,10 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
   })
+
+  test('Should return a token on sign success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_jwt')
+  })
 })
