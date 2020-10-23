d43169b557fce3bce8612e394061b74029908031
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-23 12:32:02 -0300

    refactor: modify files and folders names for better reading

--- src/main/factories/login/login.ts
+++ src/main/factories/login/login.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { Controller } from '../../../presentation/protocols'
