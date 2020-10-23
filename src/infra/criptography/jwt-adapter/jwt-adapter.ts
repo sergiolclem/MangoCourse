7a4e813d716c8828d3283831a4a2e6f2075a7645
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-22 21:12:13 -0300

    feat: ensure JwtAdapter calls sign wit correct params

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+import { Encrypter } from '../../../data/protocols/criptography/token-generator'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    jwt.sign({ id: value }, this.secret)
+    return null
+  }
+}
