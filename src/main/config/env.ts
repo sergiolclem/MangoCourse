45852b1ef9372be563aa0310631d01b175af0550
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-24 01:06:44 -0300

    feat: create login route

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || '05676d16-ebaf-42a8-9d99-606c90e7f781'
+  jwtSecret: process.env.JWT_SECRET || '42a8-9d99'
 }
