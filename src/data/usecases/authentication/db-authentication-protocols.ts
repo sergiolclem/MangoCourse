86597147ec7dd2707d7569de23f64d7349570cf3
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-26 12:50:17 -0300

    chore: update tracked files

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++
@@ -1,6 +0,0 @@
-export * from '../../../domain/usecases/authentication'
-export * from '../../../domain/models/account'
-export * from '../../protocols/criptography/hash-comparer'
-export * from '../../protocols/criptography/token-generator'
-export * from '../../protocols/db/account/load-account-by-email-repository'
-export * from '../../protocols/db/account/update-access-token'
