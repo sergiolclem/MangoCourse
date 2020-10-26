58bb4df19a90d49846697e39b1c01ff1efa9ee56
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-26 13:13:11 -0300

    feat: ensure AddAccount return null if loadByEmail finds a user

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,13 +1,17 @@
+import { LoadAccountByEmailRepository } from '../authentication/db-authentication-protocols'
 import { AccountModel, AddAccount, AddAccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
+    const alreadyExists = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (alreadyExists) { return null }
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
   }
