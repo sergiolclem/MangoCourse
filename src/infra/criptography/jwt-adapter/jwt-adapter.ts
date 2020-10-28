75c8ddf428d6c85e50a7ac2d1e0870cceaa905d4
Sérgio Luis Clem <sergiolclem@gmail.com>
2020-10-28 20:06:52 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,16 @@
 import jwt from 'jsonwebtoken'
-
+import { Decrypter } from '../../../data/protocols/criptography/decrypter'
 import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) { }
 
   encrypt (value: string): string {
     return jwt.sign({ id: value }, this.secret)
+  }
+
+  decrypt (value: string): string {
+    jwt.verify(value, this.secret)
+    return null
   }
 }
